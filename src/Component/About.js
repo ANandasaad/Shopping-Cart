2909d35b8cbc5899c3aa97ef8df090c8ef276d8e
Anand Kushwaha <98548977+ANandasaad@users.noreply.github.com>
2023-04-13 22:43:10 +0530

    done

--- src/Component/About.js
+++ src/Component/About.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+        <div className='flex  justify-center items-center'>
+            <p>Quick Shoping is A Fake Store . In this Project is used Fake Store Api </p>
+            <img className='w-96' src='https://fakestoreapi.com/icons/intro.svg'/>
+        </div>
+        
+    </div>
   )
 }
 
